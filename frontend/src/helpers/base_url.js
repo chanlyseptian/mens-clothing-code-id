29cc3a8487aa7b92035bc326119d4da8e6c4dfbb
ronaldofernandus <53988493+ronaldofernandus@users.noreply.github.com>
2022-07-21 20:34:37 +0700

    Delete frontend directory

--- frontend/src/helpers/base_url.js
+++
@@ -1,3 +0,0 @@
-const base_url = "http://localhost:3000";
-
-export default base_url;
--- frontend/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./userReducer";
-import cmsReducer from "./cmsReducer";
-import shoppingReducer from "./shoppingReducer";
-
-export default combineReducers({ userReducer, cmsReducer, shoppingReducer });
